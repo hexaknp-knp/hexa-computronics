add6e381d97218615e9337c4b8e6c07ee52ac5e5
hexaknp-knp <hexaknp@gmail.com>
2026-02-21 21:39:49 +0530

    Update script.js

--- test2/script.js
+++ test2/script.js
@@ -1,10 +1,32 @@
 let cart = JSON.parse(localStorage.getItem("cart")) || [];
 let discount = 0;
 
+let stockData = {
+"Laptop Model X":5,
+"WiFi 6 Router":10,
+"24 Port Switch":8,
+"Enterprise Firewall":3
+};
+
 function addToCart(name, price){
+if(stockData[name] <= 0){
+alert("Out of Stock");
+return;
+}
 cart.push({name, price});
+stockData[name]--;
 localStorage.setItem("cart", JSON.stringify(cart));
 alert("Added to cart");
+updateStock();
+}
+
+function updateStock(){
+document.querySelectorAll(".product").forEach(p=>{
+let name = p.getAttribute("data-name");
+if(stockData[name] !== undefined){
+p.querySelector(".stock").innerText = "Stock: "+stockData[name];
+}
+});
 }
 
 function openCart(){
@@ -30,15 +52,17 @@ cartItems.innerHTML += `
 </span>
 </div>`;
 });
-
 total = total - discount;
 document.getElementById("total").innerText="Total: ₹"+total;
 }
 
 function removeItem(index){
+let name = cart[index].name;
+stockData[name]++;
 cart.splice(index,1);
 localStorage.setItem("cart", JSON.stringify(cart));
 displayCart();
+updateStock();
 }
 
 function applyCoupon(){
@@ -54,16 +78,13 @@ displayCart();
 
 function searchProduct(){
 let input = document.getElementById("search").value.toLowerCase();
-let products = document.querySelectorAll(".product");
-products.forEach(p=>{
-let text = p.innerText.toLowerCase();
-p.style.display = text.includes(input) ? "block" : "none";
+document.querySelectorAll(".product").forEach(p=>{
+p.style.display = p.innerText.toLowerCase().includes(input) ? "block":"none";
 });
 }
 
 function filterCategory(cat){
-let products = document.querySelectorAll(".product");
-products.forEach(p=>{
+document.querySelectorAll(".product").forEach(p=>{
 if(cat==="all"){p.style.display="block";}
 else{
 p.style.display = p.getAttribute("data-category")===cat ? "block":"none";
@@ -78,6 +99,18 @@ cart.forEach(item=>{
 message += item.name + " - ₹" + item.price + "\n";
 });
 message += "\n" + total;
-
 window.open("https://wa.me/919935529306?text="+encodeURIComponent(message));
+}
+
+function downloadInvoice(){
+let content = "INVOICE\n\n";
+cart.forEach(item=>{
+content += item.name+" - ₹"+item.price+"\n";
+});
+content += "\n"+document.getElementById("total").innerText;
+let blob = new Blob([content],{type:"text/plain"});
+let link = document.createElement("a");
+link.href = URL.createObjectURL(blob);
+link.download = "invoice.txt";
+link.click();
 }
