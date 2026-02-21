c16c89a49f2a959ed7335b5cc1cbc5cb799cd0d7
hexaknp-knp <hexaknp@gmail.com>
2026-02-21 21:19:11 +0530

    Update script.js

--- test2/script.js
+++ test2/script.js
@@ -1,1 +1,39 @@
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
 
+function addToCart(name, price){
+cart.push({name, price});
+localStorage.setItem("cart", JSON.stringify(cart));
+alert("Added to cart");
+}
+
+function openCart(){
+document.getElementById("cart").style.display="block";
+displayCart();
+}
+
+function closeCart(){
+document.getElementById("cart").style.display="none";
+}
+
+function displayCart(){
+let cartItems = document.getElementById("cart-items");
+let total = 0;
+cartItems.innerHTML="";
+cart.forEach((item,index)=>{
+total += item.price;
+cartItems.innerHTML += `
+<div class="cart-item">
+<span>${item.name}</span>
+<span>₹${item.price}
+<button onclick="removeItem(${index})">X</button>
+</span>
+</div>`;
+});
+document.getElementById("total").innerText="Total: ₹"+total;
+}
+
+function removeItem(index){
+cart.splice(index,1);
+localStorage.setItem("cart", JSON.stringify(cart));
+displayCart();
+}
